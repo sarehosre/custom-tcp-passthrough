75e48135e24b49a38d2403af3f1ab542f573ab34
asisit@hotmail.com <asisit@hotmail.com@0822be52-b7be-68e0-2223-a445d7b2066f>
2012-11-05 19:46:56 +0000

    Version 2.0.0

--- trunk/gameGate.pro
+++ trunk/gameGate.pro
@@ -4,10 +4,12 @@
 #
 #-------------------------------------------------
 
+# ATTENTION: This application is only designed for Windows.
+
 QT       += core gui network
 CONFIG += network
 
-TARGET = gameGate
+TARGET = custom-tcp-passthrough
 TEMPLATE = app
 
 
@@ -17,3 +19,35 @@ SOURCES += main.cpp\
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+
+win32 {
+    RC_FILE = MyApp.rc
+    WINSDK_DIR = C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A
+    WIN_PWD = $$replace(PWD, /, \\)
+    OUT_PWD_WIN = $$replace(OUT_PWD, /, \\)
+    QMAKE_POST_LINK = "$$WINSDK_DIR/bin/mt.exe -manifest $$quote($$WIN_PWD\\$$basename(TARGET).manifest) -outputresource:$$quote($$OUT_PWD_WIN\\${DESTDIR_TARGET};1)"
+
+
+}
+
+
+documentation.path = $$OUT_PWD_WIN\\docs
+documentation.files = docs\\*
+INSTALLS += documentation
+
+
+#copyfiles.commands = @echo THIS WILL B $$DESTDIR\\docs docs\\about.txt
+#QMAKE_EXTRA_TARGETS += copyfiles
+#POST_TARGETDEPS += copyfiles
+
+
+
+#install_it.path = $$BUILDDIR/docs
+#install_it.files += $$SOURCEDIR/docs/about.txt
+
+#INSTALLS += install_it
+
+OTHER_FILES += \
+    MyApp.rc \
+    custom-tcp-passthrough.manifest
--- trunk/mainwindow.h
+++ trunk/mainwindow.h
@@ -4,6 +4,11 @@
 #include <QMainWindow>
 #include <QTcpSocket>
 #include <QTcpServer>
+#include <QUrl>
+#include <QDesktopServices>
+#include <QProcess>
+#include <QFile>
+#include <QDate>
 
 namespace Ui {
     class MainWindow;
@@ -17,18 +22,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
 private:
     Ui::MainWindow *ui;
     QTcpServer *Server;
     QTcpSocket *externalSocket;
     QTcpSocket *internalSocket;
+    QString defHost;
+    QString defPort;
+    bool reachedServer;
+    void log(QString, QString);
+    QFile *LogFile;
+    QTextStream *LogStream;
+    bool loopActEd;
 
 private slots:
+    void on_pushB_fixError_clicked();
+    void on_pushB_startNetAnal_clicked();
+    void on_pushB_exit_clicked();
+
+    void processNasState(QAbstractSocket::SocketState);
+    void processNasError(QAbstractSocket::SocketError);
+
+    void processLink(QUrl);
+
+    void runLocalServer();
     void manageInQuery();
     void readIntSocket();
     void readExtSocket();
     void manageDisco();
     void connectedToExt();
+
+    void setupLoopback(bool);
+    void finishLoopbackAct(int);
+    void finishLoopbackDeAct(int);
+
 
 };
 
